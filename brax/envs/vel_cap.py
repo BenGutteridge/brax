e476d2d4a102e2cf468a096e6327f262e1a6c991
BenGutteridge <benpgutteridge@gmail.com>
2022-05-06 01:05:44 +0100

    checking that it's not the hyperparams

--- brax/envs/vel_cap.py
+++ brax/envs/vel_cap.py
@@ -87,13 +87,13 @@ class VelCap(env.Env):
     p2_ball_vec /= p2_ball_dist_before
     # get force magnitude multiplier from action,
     # scale down force based on distance from ball
-    ball_thrusters = False
+    ball_thrusters = True
     if ball_thrusters:
       max_dist = 10. # max distance from ball that can still exert force
       p1_force_mult = action[0] * (1 - (p1_ball_dist_before/max_dist)**2)
       p2_force_mult = action[1] * (1 - (p2_ball_dist_before/max_dist)**2)
-      # p1_force_mult = jp.clip(p1_force_mult, 0, jp.inf)
-      # p2_force_mult = jp.clip(p2_force_mult, 0, jp.inf)
+      p1_force_mult = jp.clip(p1_force_mult, 0, jp.inf)
+      p2_force_mult = jp.clip(p2_force_mult, 0, jp.inf)
       # get acceleration vectors
       p1_ball_acc = p1_force_mult * p1_ball_vec
       p2_ball_acc = p2_force_mult * p2_ball_vec
