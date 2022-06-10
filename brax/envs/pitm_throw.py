4db0aab902ce3f8d06756cb7943e998d78026814
BenGutteridge <benpgutteridge@gmail.com>
2022-06-10 13:40:25 +0100

    indexing bug with ball passing reward

--- brax/envs/pitm_throw.py
+++ brax/envs/pitm_throw.py
@@ -150,9 +150,8 @@ class PITM_Throw(env.Env):
 
     # Reward for ball passing from current player to one of the others
     scale = 20
-    other_player_poses = self.player_poses
-    other_player_poses.pop(state.metrics['previous_player_idx'])
-    ball_player_deltas = jp.array([norm(ball_pos_before - pos) - norm(ball_pos_after - pos) for pos in other_player_poses])
+    ball_player_deltas = jp.array([norm(ball_pos_before - pos) - norm(ball_pos_after - pos) for pos in self.player_poses])
+    ball_player_deltas[state.metrics['previous_player_idx']] = -jnp.inf # don't count self
     # +ve is towards player
     ball_passing_reward = jnp.max(ball_player_deltas) / self.sys.config.dt * scale
 
