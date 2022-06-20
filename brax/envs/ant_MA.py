00a26f55a955ed535c1771a63492e2188aecfed5
BenGutteridge <benpgutteridge@gmail.com>
2022-06-20 20:38:12 +0100

    adding kwarg for go_x_dir to ant_MA

--- brax/envs/ant_MA.py
+++ brax/envs/ant_MA.py
@@ -26,9 +26,9 @@ class Ant_MA(env.Env):
 
   def __init__(self, legacy_spring=False, **kwargs):
     config = _SYSTEM_CONFIG_SPRING if legacy_spring else _SYSTEM_CONFIG
-    super().__init__(config=config, **kwargs)
     is_multiagent = False if kwargs.pop('is_not_multiagent', False) else True
     self.go_x_dir = bool(kwargs.pop('go_x_dir', False))
+    super().__init__(config=config, **kwargs)
     if is_multiagent:
       self.n_agents, self.actuators_per_agent = 2, 4
       players = ['agent_%d' % i for i in range(self.n_agents)]
