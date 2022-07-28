d2d3401a75032db4f644dee2309de53f4daa3310
BenGutteridge <benpgutteridge@gmail.com>
2022-07-28 12:49:44 +0100

    debug print checking if gru cell is learning

--- brax/training/ppo.py
+++ brax/training/ppo.py
@@ -277,6 +277,8 @@ def train(
   optimizer = optax.adam(learning_rate=learning_rate)
   init_params = {'policy': policy_model.init(key_policy),
                  'value': value_model.init(key_value)}
+  print("Checking if GRU params are updating:\ninit_params['policy']:\n", init_params['policy'])
+  
   optimizer_state = optimizer.init(init_params)
   optimizer_state, init_params = pmap.bcast_local_devices(
       (optimizer_state, init_params), local_devices_to_use)
