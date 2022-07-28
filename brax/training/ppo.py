5dad30ec6e408e419c98c512043a55e69e9d6817
BenGutteridge <benpgutteridge@gmail.com>
2022-07-28 17:37:19 +0100

    fixing initialised hidden states to be size of hard coded memory, instead of size of NN output

--- brax/training/ppo.py
+++ brax/training/ppo.py
@@ -275,7 +275,7 @@ def train(
       val_num_hidden_layers=val_num_hidden_layers,
       val_num_neurons_per_layer=val_num_neurons_per_layer,
       recurrent=recurrent, ################## BEN ADDITION ######################
-      memory_size=recurrent_memory_size,
+      memory_size=memory_size,
       )
   key_policy, key_value = jax.random.split(key_models)
 
