3d1d7cdcba95e639cc01a82353f10a1874412da3
BenGutteridge <benpgutteridge@gmail.com>
2022-08-02 14:44:21 +0100

    put layernorm back in

--- brax/training/networks.py
+++ brax/training/networks.py
@@ -56,7 +56,7 @@ class GRU_MLP(linen.Module):
           kernel_init=self.kernel_init,
           use_bias=self.bias)(output)
       if i == penultimate:
-        # output = linen.LayerNorm()(output) # normalising inputs so we don't saturate tanh in GRU cell
+        output = linen.LayerNorm()(output) # normalising inputs so we don't saturate tanh in GRU cell
         hidden, output = linen.GRUCell(
                                       #  gate_fn=linen.relu, 
                                       #  activation_fn=linen.relu, 
