30abc59f7d37c0cb91525dc49a6ab9e236fbda26
BenGutteridge <benpgutteridge@gmail.com>
2022-08-03 18:18:11 +0100

    fixing bugs with rnn implementation in mappo

--- brax/experimental/composer/training/mappo.py
+++ brax/experimental/composer/training/mappo.py
@@ -447,7 +447,11 @@ def train(environment_fn: Callable[..., envs.Env],
       nstate = step_fn(state, postprocessed_actions, normalizer_params,
                       extra_params)
 
-      counter = counter.at[state.core.info['agent_idx']].add(1) # BEN
+      try:
+        counter = counter.at[state.core.info['agent_idx']].add(1) # BEN
+      except:
+        print('Non-BR - no counter')
+        pass
 
       return (nstate, normalizer_params, policy_params, extra_params, key, 
               counter, # BEN
